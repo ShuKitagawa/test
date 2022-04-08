e825066b26c3ffe73c7170411b16ff8d96c08aca
Shu kitagawa <pirooporoo@gmail.com>
2022-04-08 13:28:52 +0900

    機能の分岐を追加

--- untitled/src/carManagement.java
+++ untitled/src/carManagement.java
@@ -1,12 +1,62 @@
 import java.util.Scanner;
 
 public class carManagement {
-    public static void main() {
-    	System.out.println("登録する車種を入力してください。");
-    	String newCar = new Scanner(System.in).nextLine();
-    	dbCon.sqlexcute(newCar);
-    	System.out.println(newCar + "を登録しました");
-    }
-    
+	public static void main() {
+		boolean menuFlg = true;
+		while(menuFlg) {
+			System.out.println("機能を選んでください  1:車種登録 2:車種参照 3:車種削除" );
+			int fn = new java.util.Scanner(System.in).nextInt();
 
+			try {
+				switch (fn) {
+				case 1:
+					//車種登録
+					System.out.println("車を登録します");
+					carRegist();
+					break;
+
+				case 2:
+					//ToDo 車種参照
+					System.out.println("登録した車を確認します");
+					break;
+
+				case 3:
+					// ToDo 車種削除
+					System.out.println("登録した車を削除します");
+					break;
+
+				default:
+					System.out.println("不正な値が入力されました");
+				}
+
+
+				System.out.println("車種管理メニューに戻りますか？ 1:Yes 2:No");
+				int mn = new java.util.Scanner(System.in).nextInt();
+
+				//終了処理
+				if(mn == 2){
+					System.out.println("終了します");
+					menuFlg = false;
+				}
+			}catch (Exception e) {
+				System.out.println("不正な値が入力されました");
+			}
+		}
+
+	}
+
+	public static void carRegist() {
+		Boolean scanFlg = true;
+		while(scanFlg) {
+			System.out.println("登録する車種を入力してください。");
+			String newCar = new Scanner(System.in).nextLine();
+			if(newCar!= "") {
+				dbCon.sqlexcute(newCar);
+				System.out.println(newCar + "を登録しました");
+				break;
+			} else {
+				System.out.println("空白で登録することはできません。");
+			}
+		}
+	}
 }
